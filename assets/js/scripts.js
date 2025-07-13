110243358d7092c53ab9e6aed79a05728e0f2cae
Ewuawo <ewuawo@gmail.com>
2025-07-12 23:02:38 -0300

    Falta los estilos de Clientes y productos

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (e) {
+        const nombre = document.getElementById("nombre").value.trim();
+        const apellido = document.getElementById("apellido").value.trim();
+        const telefono = document.getElementById("telefono").value.trim();
+
+        if (!nombre || !apellido || !telefono) {
+            alert("Por favor, completa todos los campos.");
+            e.preventDefault();
+            return;
+        }
+        if (!/^\d{15}$/.test(telefono)) {
+            alert("El número de teléfono debe tener 15 dígitos.");
+            e.preventDefault();
+            return;
+        }
+
+        alert("Cliente agregado correctamente.");
+    })
+})
